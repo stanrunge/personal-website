b12cdb75930c10297329cc76563751391e1db4cc
Stan <65637274+stanrunge@users.noreply.github.com>
2022-07-05 21:26:28 +0200

    further improvements

--- pages/index.js
+++ pages/index.js
@@ -18,15 +18,6 @@ export default function Home() {
                     <meta name="viewport" content="initial-scale=1, width=device-width"/>
                     <link rel="icon" href="/favicon.ico"/>
 
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                    />
-
                     <script async
                             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9928063674893352"
                             crossOrigin="anonymous"></script>
@@ -35,8 +26,8 @@ export default function Home() {
                 <h1>Stan Runge</h1>
 
                 <h2>Projects</h2>
-                <ActionAreaCard title={"CES Gaming"} description={"Organization for hosting videogame tournaments."}/>
-                <ActionAreaCard title={"OpenBionicReading"} description={"Chrome plugin for easier text reading."}/>
+                <ActionAreaCard title={"CES Gaming"} description={"Organization for hosting videogame tournaments."} image={"/images/cesgaming.png"}/>
+                <ActionAreaCard title={"OpenBionicReading"} description={"Chrome plugin for easier text reading."} image={"/images/openbionicreading.png"}/>
             </div>
         </Layout>
     )
