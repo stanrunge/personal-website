6ce8980d814f7b0d7b6a1f425d566195ed5bf310
Stan <stanrunge03@gmail.com>
2022-07-05 03:09:51 +0200

    added new basic pages

---
+++ pages/projects/pp-calculator.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout";
+
+export default function PP_Calculator() {
+    return (
+        <Layout home={false}>
+            <h1>PP Calculator</h1>
+            <p>osu! pp calculator for different reworks</p>
+        </Layout>
+    )
+}
