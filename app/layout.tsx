f1dac2559e612eef3271c37b3ba3cfdf26ced66e
Stan Runge <stan@stanrunge.dev>
2024-01-14 17:13:02 +0100

    Delete old files and create new nextjs+supabase project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,28 @@
-import type { Metadata } from 'next'
+import { GeistSans } from "geist/font/sans";
+import "./globals.css";
 
-export const metadata: Metadata = {
-  title: 'Stan Runge',
-  description: 'Stan Runge',
-}
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+      </body>
     </html>
-  )
+  );
 }
