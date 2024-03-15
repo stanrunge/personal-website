fb76d08ff5ac72956aec7fdf0d79d6748c57a556
Stan Runge <stan@stanrunge.dev>
2024-03-15 02:25:07 +0100

    feat: add basic info and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
 export default async function Index() {
-  return <>Hello</>;
+  return (
+    <div>
+      <div className="flex items-center place-content-center gap-10 py-10">
+        <img src="stan.jpg" alt="Stan Runge" className="rounded-xl size-64" />
+        <h1 className="text-5xl">Stan Runge</h1>
+      </div>
+
+      <div className="text-center my-10">
+        <h2>Experience</h2>
+        <b>Founder</b>
+        <p>Vash Software</p>
+      </div>
+      <div className="text-center my-10">
+        <b>Software Engineer Intern</b>
+        <p>H5mag</p>
+      </div>
+      <div className="text-center my-10">
+        <b>Junior IT Specialist</b>
+        <p>Finance Plus</p>
+      </div>
+      <div className="text-center my-10">
+        <h2>Education</h2>
+        <b>The Hague University of Applied Sciences</b>
+      </div>
+    </div>
+  );
 }
