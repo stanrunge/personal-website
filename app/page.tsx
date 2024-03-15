e389d13edd03e89bcb2c66652762942537d25b4d
Stan Runge <stan@stanrunge.dev>
2024-03-15 02:05:28 +0100

    fix: clean src

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,3 @@
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
-
 export default async function Index() {
-  const cookieStore = cookies();
-
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
-  return (
-    <>
-      <h1 className="text-xl">hello</h1>
-    </>
-  );
+  return <>Hello</>;
 }
