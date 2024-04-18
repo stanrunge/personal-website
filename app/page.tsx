1b8d84b46940c89a86ac9572c08e4b0f1051f62c
Stan Runge <stan@stanrunge.dev>
2024-04-19 01:01:18 +0200

    Add certificates section to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,18 @@ export default async function Index() {
       </div>
 
       <div className="text-center my-10">
+        <h2 className="text-3xl">Certificates</h2>
+        <b>Mathematics for Machine Learning: Linear Algebra</b>
+        <p>Imperial College London</p>
+        <a
+          className="text-blue-500"
+          href="https://coursera.org/share/11b0e4d6cf00b44869dcf9c4bec70865"
+        >
+          Link to Certificate
+        </a>
+      </div>
+
+      <div className="text-center my-10">
         <h2 className="text-3xl">Links</h2>
         <ul>
           <li>
