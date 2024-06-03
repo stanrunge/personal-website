3762f6ba26559eb97e349aea852d7fee2bec95ec
Stan Runge <stan@stanrunge.dev>
2024-06-03 17:46:29 +0200

    move blog to publications

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Index() {
         <img src="stan.jpg" alt="Stan Runge" className="rounded-xl size-64" />
         <div className="text-center">
           <h1 className="text-5xl my-3">Stan Runge</h1>
-          <p>stan@stanrunge.dev</p>
+          <a href="mailto:stan@stanrunge.dev">stan@stanrunge.dev</a>
           <p>The Hague, Netherlands</p>
         </div>
       </div>
@@ -28,6 +28,27 @@ export default async function Index() {
         <b>The Hague University of Applied Sciences</b>
         <p>BSc Software Engineering</p>
         <p>Minor: Deep Learning</p>
+      </div>
+
+      <div className="text-center my-10">
+        <h2 className="text-3xl">Skills</h2>
+        <div className="text-center my-10">
+          <b>Programming Languages</b>
+          <p>JavaScript, TypeScript, Python, C#, SQL</p>
+        </div>
+
+        <div className="text-center my-10">
+          <b>Frameworks/Libraries</b>
+          <p>
+            React, Next.js, Node.js, .NET, SvelteKit, SQL Server, PostgreSQL,
+            MySQL
+          </p>
+        </div>
+
+        <div className="text-center my-10">
+          <b>Natural Languages</b>
+          <p>English, Dutch, Spanish, Papiamentu, Korean</p>
+        </div>
       </div>
 
       <div className="text-center my-10">
