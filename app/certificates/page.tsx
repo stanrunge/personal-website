c527eebd2b50bb2f634a710a42cb5b9a29a5add0
Stan Runge <stan@stanrunge.dev>
2024-06-03 17:56:26 +0200

    update deps and add blank pages

---
+++ app/certificates/page.tsx
@@ -0,0 +1,19 @@
+export default async function Page() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl">Certificates (1)</h1>
+
+      <div className="text-center my-10">
+        <h2 className="text-3xl">Certificates</h2>
+        <b>Mathematics for Machine Learning: Linear Algebra</b>
+        <p>Imperial College London</p>
+        <a
+          className="text-blue-500"
+          href="https://coursera.org/share/11b0e4d6cf00b44869dcf9c4bec70865"
+        >
+          Link to Certificate
+        </a>
+      </div>
+    </div>
+  );
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,23 +1,8 @@
 export default async function Page() {
-  return (
-    <div className="flex flex-col items-center">
-      <div className="my-3 flex flex-col items-center">
-        <h1 className="text-4xl">Stan's Blog</h1>
-        <p className="text-sm my-1">
-          <i>but what do I know</i>
-        </p>
+    return (
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl">Projects (2)</h1>
       </div>
-
-      <h2 className="mt-3 text-2xl">Posts ({[].length})</h2>
-      {/* 
-        <ul>
-          <li>
-            <a href="/blog/neural-nets">Neural Networks</a>
-          </li>
-          <li>
-            <a href="/blog/nextjs">Next.js</a>
-          </li>
-        </ul> */}
-    </div>
-  );
-}
+    );
+  }
+  
