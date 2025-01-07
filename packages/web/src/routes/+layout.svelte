2131df26b28c62dbf4eac67769e50f47f80b31b7
Stan Runge <stan@stanrunge.dev>
2025-01-07 13:30:55 +0100

    use new sveltekit project

---
+++ packages/web/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<nav class="flex justify-around">
+	<div><a href="/">Stan Runge</a></div>
+	<div>
+		<a href="/progress">Progress</a>
+	</div>
+</nav>
+
+<slot />
--- packages/web/src/routes/+page.server.ts
+++ packages/web/src/routes/+page.server.ts
@@ -5,6 +5,6 @@ import type { PageServerLoad } from './$types';
 export const load = (async () => {
 	// const result = await db.select().from(myTable);
 	return {
-		message: 'Hi :3'
+		message: 'Hi :4888829383991292222244343434'
 	};
 }) satisfies PageServerLoad;
