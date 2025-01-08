c302efc18cf5277ae1d54074a78d74b2db9f5d7e
Stan Runge <stan@stanrunge.dev>
2025-01-08 04:09:54 +0100

    tasks to topics relations

--- packages/web/src/lib/db/schema.ts
+++ packages/web/src/lib/db/schema.ts
@@ -1,9 +1,21 @@
+import { relations } from 'drizzle-orm';
 import { integer, pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 const client = postgres('postgresql://postgres:postgres@db:5432/db');
 export const db = drizzle(client, {});
+
+export const tasks = pgTable('tasks', {
+	id: serial('id').primaryKey(),
+	name: varchar('name', { length: 256 }),
+	createdAt: timestamp('created_at').defaultNow(),
+	updatedAt: timestamp('updated_at').defaultNow()
+});
+
+export const tasksRelations = relations(tasks, ({ many }) => ({
+	tasksToTopics: many(tasksToTopics)
+}))
 
 export const topics = pgTable('topics', {
 	id: serial('id').primaryKey(),
@@ -12,6 +24,30 @@ export const topics = pgTable('topics', {
 	createdAt: timestamp('created_at').defaultNow(),
 	updatedAt: timestamp('updated_at').defaultNow()
 });
+
+export const topicsRelations = relations(topics, ({ many }) => ({
+	topicsToTasks: many(tasksToTopics)
+}));
+
+export const tasksToTopics = pgTable('tasks_to_topics', {
+	taskId: integer('task_id')
+		.notNull()
+		.references(() => tasks.id),
+	topicId: integer('topic_id')
+		.notNull()
+		.references(() => topics.id)
+})
+
+export const taskToTopicsRelations = relations(tasksToTopics, ({ one }) => ({
+	task: one(tasks, {
+		fields: [tasksToTopics.taskId],
+		references: [tasks.id]
+	}),
+	topic: one(topics, {
+		fields: [tasksToTopics.topicId],
+		references: [topics.id]
+	})
+}));
 
 export const universities = pgTable('universities', {
 	id: serial('id').primaryKey(),
@@ -35,12 +71,4 @@ export const publications = pgTable('publications', {
 	createdAt: timestamp('created_at').defaultNow(),
 	updatedAt: timestamp('updated_at').defaultNow()
 });
-
-export const tasks = pgTable('topics', {
-	id: serial('id').primaryKey(),
-	name: varchar('name', { length: 256 }),
-	createdAt: timestamp('created_at').defaultNow(),
-	updatedAt: timestamp('updated_at').defaultNow()
-});
-
 
