b679e259ed7ade84373e8a3499ef2cb95322ccd6
Stan Runge <stan@stanrunge.dev>
2025-01-18 01:21:00 +0100

    temp: check db url

--- packages/web/src/lib/db/index.ts
+++ packages/web/src/lib/db/index.ts
@@ -3,6 +3,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { DB_URL } from '$env/static/private';
 import postgres from 'postgres';
 
+console.log('DB_URL', DB_URL);
 const client = postgres(DB_URL, { prepare: false });
 export const db = drizzle({
 	schema,
